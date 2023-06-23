acdeef1bb96e8c8ffc76ab5bb824b1da1ef16f47
Diletta Pelloni <dilpel@live.it>
2023-06-23 15:10:29 +0200

    Controller con filtro completato

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -22,13 +23,18 @@ public class PizzaController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index(Model model) {
-        //prendo la lista di pizze
-        List<Pizza> pizzas = pizzaRepository.findAll();
-        //passo la lista al model
-        model.addAttribute("pizzas", pizzas);
-        //ritorno la view
-        return "pizza/index";
+    public String index(
+        Model model,
+        @RequestParam(name= "keyword", required = false) String keyword //gestisco la presenza di un eventuale parametro keyword nella richiesta
+    ) {
+        List<Pizza> pizzas; //variabile da restituire
+        if(keyword == null || keyword.isBlank()) { //se il parametro non c'è o è una stringa vuota
+            pizzas = pizzaRepository.findAll(); //restituirò l'intera lista di pizze
+        } else { //se mi è stata data la keyword da ricercare
+            pizzas = pizzaRepository.findByNameContainingIgnoreCase(keyword); //la passo al metodo che la utilizzerà per fare la QUERY
+        }
+        model.addAttribute("pizzas", pizzas); //passo la lista al model
+        return "pizza/index"; //ritorno la view
     }
 
     @GetMapping("/{id}")
--- src/main/java/org/lessons/springlamiapizzeriacrud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/repository/PizzaRepository.java
@@ -3,5 +3,11 @@ package org.lessons.springlamiapizzeriacrud.repository;
 import org.lessons.springlamiapizzeriacrud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
+    //cerca le pizze il cui nome contiene una stringa ignorando maiuscole/minuscole
+    List<Pizza> findByNameContainingIgnoreCase(String keyword);
+
 }
