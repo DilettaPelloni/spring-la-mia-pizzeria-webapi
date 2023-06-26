ccab659f2fa6758fed99c05607f2b5734b3b5249
Diletta Pelloni <dilpel@live.it>
2023-06-26 14:50:43 +0200

    Impostato metodo store funzionante

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Optional;
@@ -58,6 +55,14 @@ public class PizzaController {
     public String create(Model model) {
         model.addAttribute("pizza", new Pizza());
         return "pizza/create";
+    }
+
+    @PostMapping("/create")
+    public String store(
+        @ModelAttribute("pizza") Pizza formPizza
+    ) {
+        pizzaRepository.save(formPizza);
+        return "redirect:/pizzas";
     }
 
 }
