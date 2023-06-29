ed4ac274b6be899a5460b80c62f0ae30ac555747
Diletta Pelloni <dilpel@live.it>
2023-06-29 14:24:23 +0200

    Creata entity Offer

--- src/main/java/org/lessons/springlamiapizzeriacrud/model/Offer.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/model/Offer.java
@@ -1,6 +1,10 @@
 package org.lessons.springlamiapizzeriacrud.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "offers")
@@ -10,6 +14,48 @@ public class Offer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "Title must not be null or blank")
+    @Size(min=3, max=255, message = "Name must have min 3 and max 255 characters")
+    private String Title;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
 
 
+    //GETTERS & SETTERS -------------------------------------------------------------------------
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
