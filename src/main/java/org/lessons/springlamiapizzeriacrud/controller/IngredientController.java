685555b321993e48f0f1206b73faff9730f95fd6
Diletta Pelloni <dilpel@live.it>
2023-06-30 14:44:37 +0200

    fatta create

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/IngredientController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/IngredientController.java
@@ -1,12 +1,19 @@
 package org.lessons.springlamiapizzeriacrud.controller;
 
+import jakarta.validation.Valid;
+import org.lessons.springlamiapizzeriacrud.messages.AlertMessage;
+import org.lessons.springlamiapizzeriacrud.messages.AlertMessageType;
 import org.lessons.springlamiapizzeriacrud.model.Ingredient;
 import org.lessons.springlamiapizzeriacrud.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -19,10 +26,27 @@ public class IngredientController {
 
     @GetMapping
     public String index(Model model) {
-        List<Ingredient> ingredients = ingredientRepository.findAll(); //per mostrare l'elenco degli ingredienti
         model.addAttribute("ingObj", new Ingredient()); //per l'editor
+        List<Ingredient> ingredients = ingredientRepository.findAll(); //per mostrare l'elenco degli ingredienti
         model.addAttribute("ingredients", ingredients);
         return "/ingredient/index";
+    }
+
+    @PostMapping("/save")
+    public String save(
+        @Valid @ModelAttribute Ingredient formIngredient,
+        BindingResult bindingResult,
+        Model model,
+        RedirectAttributes redirectAttributes
+    ) {
+        if(bindingResult.hasErrors()) { //se ci sono errori
+            List<Ingredient> ingredients = ingredientRepository.findAll();
+            model.addAttribute("ingredients", ingredients);
+            return "/ingredient/index";
+        }
+        ingredientRepository.save(formIngredient);
+        redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "Ingredient " + formIngredient.getName() + " created successfully!"));
+        return "redirect:/ingredients";
     }
 
 }
