33106cde953987f4684d50d9c95c6a94413fad7f
Diletta Pelloni <dilpel@live.it>
2023-06-30 14:48:21 +0200

    aggiunto controllo univocità

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/IngredientController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/IngredientController.java
@@ -4,11 +4,13 @@ import jakarta.validation.Valid;
 import org.lessons.springlamiapizzeriacrud.messages.AlertMessage;
 import org.lessons.springlamiapizzeriacrud.messages.AlertMessageType;
 import org.lessons.springlamiapizzeriacrud.model.Ingredient;
+import org.lessons.springlamiapizzeriacrud.model.Pizza;
 import org.lessons.springlamiapizzeriacrud.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -34,11 +37,21 @@ public class IngredientController {
 
     @PostMapping("/save")
     public String save(
-        @Valid @ModelAttribute Ingredient formIngredient,
+        @Valid @ModelAttribute("ingObj") Ingredient formIngredient,
         BindingResult bindingResult,
         Model model,
         RedirectAttributes redirectAttributes
     ) {
+        if(!isNameUnique(formIngredient.getName())) { //se il nome NON è univoco
+            bindingResult.addError(new FieldError(
+                    "ingObj",
+                    "name",
+                    formIngredient.getName(),
+                    false,null,null,
+                    "Name must be unique."
+            ));
+
+        }
         if(bindingResult.hasErrors()) { //se ci sono errori
             List<Ingredient> ingredients = ingredientRepository.findAll();
             model.addAttribute("ingredients", ingredients);
@@ -47,6 +60,12 @@ public class IngredientController {
         ingredientRepository.save(formIngredient);
         redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "Ingredient " + formIngredient.getName() + " created successfully!"));
         return "redirect:/ingredients";
+    }
+
+    //UTILITIES ----------------------------------------------------------------------------------------
+    private boolean isNameUnique(String name){
+        Optional<Ingredient> result = ingredientRepository.findByName(name);
+        return result.isEmpty();
     }
 
 }
--- src/main/java/org/lessons/springlamiapizzeriacrud/repository/IngredientRepository.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/repository/IngredientRepository.java
@@ -1,7 +1,12 @@
 package org.lessons.springlamiapizzeriacrud.repository;
 
 import org.lessons.springlamiapizzeriacrud.model.Ingredient;
+import org.lessons.springlamiapizzeriacrud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+    //cerco una pizza tramite il nome(dato univoco)
+    Optional<Ingredient> findByName(String name);
 }
