70f33f065f3fa7c6917f9cb304cdc14035c5e178
Diletta Pelloni <dilpel@live.it>
2023-07-03 16:12:01 +0200

    Esercizio completato

---
+++ src/main/java/org/lessons/springlamiapizzeriacrud/security/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package org.lessons.springlamiapizzeriacrud.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    DbUserDetailService userDetailsService() {
+        return new DbUserDetailService();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
+                .requestMatchers("/pizzas").hasAnyAuthority("ADMIN","USER")
+                .requestMatchers("/pizzas/create").hasAuthority("ADMIN")
+                .requestMatchers("/pizzas/edit/**").hasAuthority("ADMIN")
+                .requestMatchers("/ingredients").hasAuthority("ADMIN")
+                .requestMatchers("/offers/**").hasAuthority("ADMIN")
+                .requestMatchers("/pizzas/**").hasAnyAuthority("ADMIN","USER")
+                .requestMatchers(("/**")).permitAll()
+                .and().formLogin()
+                .and().logout();
+        return http.build();
+    }
+
+}
