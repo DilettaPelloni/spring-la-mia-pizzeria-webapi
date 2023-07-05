f2b8584d37a04b4a880fa0c8bce83bd6d8376b8d
Diletta Pelloni <dilpel@live.it>
2023-07-05 17:10:02 +0200

    Esercizio completato

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
@@ -30,25 +30,16 @@ public class PizzaController {
     @Autowired
     private PizzaService pizzaService;
     @Autowired
-    private PizzaRepository pizzaRepository;
-    @Autowired
     private IngredientRepository ingredientRepository;
 
     //READ --------------------------------------------------------------------------------------------------------------
     @GetMapping
     public String index(
         Model model,
-        @RequestParam(name= "keyword", required = false) String keyword //gestisco la presenza di un eventuale parametro keyword nella richiesta
+        @RequestParam Optional<String> keyword
     ) {
-        List<Pizza> pizzas; //variabile da restituire
-        if(keyword == null || keyword.isBlank()) { //se il parametro non c'è o è una stringa vuota
-            pizzas = pizzaRepository.findAll(); //restituirò l'intera lista di pizze
-        } else { //se mi è stata data la keyword da ricercare
-            pizzas = pizzaRepository.findByNameContainingIgnoreCase(keyword); //la passo al metodo che la utilizzerà per fare la QUERY
-            model.addAttribute("keyword", keyword); //passo la keyword, in modo da poterla mostrare in pagina
-        }
-        model.addAttribute("pizzas", pizzas); //passo la lista al model
-        return "pizza/index"; //ritorno la view
+        model.addAttribute("pizzas", pizzaService.getAll(keyword));
+        return "pizza/index";
     }
 
     @GetMapping("/{id}")
@@ -56,9 +47,12 @@ public class PizzaController {
         Model model,
         @PathVariable("id") Integer id
     ) {
-        Pizza pizza = getPizzaById(id); //uso un metodo che cerca la pizza per id e me la restituisce, se non la trova lancia un'eccezione
-        model.addAttribute("pizza", pizza); //se ho trovato qualcosa do la pizza alla view
-        return "/pizza/show";
+        try {
+            model.addAttribute("pizza", pizzaService.getById(id));
+            return "/pizza/show";
+        } catch (PizzaNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found");
+        }
     }
 
     //CREATE --------------------------------------------------------------------------------------------------------------
@@ -78,7 +72,7 @@ public class PizzaController {
     ) {
         try {
             Pizza pizza = pizzaService.create(formPizza, bindingResult);
-            redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "Pizza " + formPizza.getName() + " created successfully!"));
+            redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "Pizza " + pizza.getName() + " created successfully!"));
             return "redirect:/pizzas/" + pizza.getId();
         } catch (InvalidAttributeValueException e) {
             model.addAttribute("ingList", ingredientRepository.findAll());
@@ -112,7 +106,7 @@ public class PizzaController {
     ) {
         try {
             Pizza pizza = pizzaService.update(id, formPizza, bindingResult);
-            redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "Pizza " + formPizza.getName() + " updated successfully!"));
+            redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "Pizza " + pizza.getName() + " updated successfully!"));
             return "redirect:/pizzas/" + pizza.getId();
         } catch (PizzaNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id "+id+" not found");
@@ -128,27 +122,13 @@ public class PizzaController {
         @PathVariable("id") Integer id,
         RedirectAttributes redirectAttributes
     ) {
-        Pizza pizza = getPizzaById(id);
-        pizzaRepository.delete(pizza);
-        redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "Pizza " + pizza.getName() + " deleted successfully!"));
-        return "redirect:/pizzas";
-    }
-
-    //UTILITIES ----------------------------------------------------------------------------------------
-    private boolean isNameUnique(String name){
-        Optional<Pizza> result = pizzaRepository.findByName(name);
-        return result.isEmpty();
-    }
-    private Pizza getPizzaById(Integer id) {
-        Optional<Pizza> pizza = pizzaRepository.findById(id); //cerco la pizza in base all'id
-        if(pizza.isEmpty()) { // se non la trovo lancio un'eccezione che restituirà un errore 404
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, //tipo di errore
-                    "Pizza with id " + id + " not found" //messaggio
-            );
+        try {
+            pizzaService.deleteById(id);
+            redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "Pizza with id "+id+" deleted successfully!"));
+            return "redirect:/pizzas";
+        } catch (PizzaNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id "+id+" not found");
         }
-        return pizza.get(); //se la trovo, restituisco la pizza
     }
-
 
 }
